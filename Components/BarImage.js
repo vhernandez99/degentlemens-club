9abcb605def33471180998fb99797bbf285ad996
vhernandez.99@hotmail.com <vhernandez.99@hotmail.com>
2022-02-18 09:32:50 -0600

    Changed main image

--- Components/BarImage.js
+++ Components/BarImage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import bar from '../assets/images/bayc-mutant-hero.jpg';
+import bar from '../assets/images/mail.png';
 const ImageBar = () => {
     return ( 
         <div id='home' data-aos="fade-up" className="neondiv text-white ni:rounded-lg ni:mb-5 pt-8 pr-2 bg-Containers-0 pb-8 xl:ml-52 xl:mr-52 xlmin:ml-0 xlmin:mr-0 border-t-2 border-t-Containers-10 border-b-2 border-b-Containers-10">
